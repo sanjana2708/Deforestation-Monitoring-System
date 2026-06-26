e138530ca817a6af55c368ade6d4b879f851a436
sanjana2708 <s.sanjana2708@gmail.com>
2026-06-26 16:29:54 +0530

    Fix: Sign Up page backdrop

--- frontend/src/constants/authAssets.js
+++ frontend/src/constants/authAssets.js
@@ -3,4 +3,4 @@ export const AUTH_IMAGE_SIGNIN =
   'https://images.unsplash.com/photo-1439066615861-d1af74d74000?auto=format&fit=crop&w=1400&q=80'
 
 export const AUTH_IMAGE_SIGNUP =
-  'https://images.unsplash.com/photo-1518173946689-a1c452b87f75?auto=format&fit=crop&w=1400&q=80'
+  'https://media.cntraveler.com/photos/5eb18e42fc043ed5d9779733/master/pass/BlackForest-Germany-GettyImages-147180370.jpg'
